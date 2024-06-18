b282d014e0ec2d5671b8d5d614ccb7eb0276507c
Luis Victoria <v@lambda.lv>
2024-06-17 21:19:26 -0400

    Solve palindrome-number

---
+++ 9.palindrome-number.cpp
@@ -0,0 +1,11 @@
+// @leet start
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string str = to_string(x);
+        string rev = str;
+        reverse(rev.begin(), rev.end());
+        return str == rev;
+    }
+};
+// @leet end
